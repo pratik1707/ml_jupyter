e62859a69491aa1ffa758c067fceaf9dfe516668
pratik1707 <pratik.malang@gmail.com>
2024-04-02 12:30:03 -0400

    Update sklearn_iris_dataset.py

--- sklearn_iris_dataset.py
+++ sklearn_iris_dataset.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
 
 from sklearn.datasets import load_iris
 iris = load_iris()
-
-
-# In[34]:
 
 
 X = iris.data
@@ -19,16 +14,11 @@ print(iris.target_names )
 type(X)
 
 
-# In[29]:
-
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 print(X_train.shape)
 print(X_test.shape)
-
-
-# In[32]:
 
 
 from sklearn.neighbors import KNeighborsClassifier
@@ -43,14 +33,9 @@ y_pred = knn.predict(X_test)
 
 
 
-# In[33]:
-
-
 from sklearn import metrics 
 print(metrics.accuracy_score(y_test, y_pred))
 
-
-# In[36]:
 
 
 sample = [[3,5,4,2], [2,3,6,4]]
@@ -59,56 +44,16 @@ pred_species = [iris.target_names[p] for p in predictions]
 print(pred_species)
 
 
-
-# In[41]:
-
-
 from joblib import dump, load
 dump(knn, 'mlbrain.joblib')
 
-
-# In[43]:
-
-
 model = load('mlbrain.joblib')
 model.predict(X_test)
-
-
-# In[45]:
 
 
 sample = [[3,5,4,2], [2,3,6,4]]
 predictions = model.predict(sample)
 pred_species = [iris.target_names[p] for p in predictions]
 print(pred_species)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
